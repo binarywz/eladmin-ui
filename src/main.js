dec76c2153a8a0661133d7e4d32e8c848dea836f
binarywz <binarywz@163.com>
2024-01-18 20:27:12 +0800

    登录页面布局

--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// global css
+import './assets/styles/index.scss'
 
+Vue.use(Element)
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app') // 手动挂载app元素
